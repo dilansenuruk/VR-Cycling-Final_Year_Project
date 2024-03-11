bb98ec4ab83e7d779e62826082aa728fa35f16e5
NaduniDamsariLiyanage <81697428+NaduniDamsariLiyanage@users.noreply.github.com>
2024-03-11 11:39:35 +0530

    udp delay statistics

--- delay_plots.py
+++ delay_plots.py
@@ -77,7 +77,7 @@ async def udp_client_ready(client_socket):
 async def main():
     global client
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_ip = "65.2.140.28"
+    server_ip = "3.109.133.185"
     server_port = 5500
     client.bind(('0.0.0.0', 5400))
     client_id = "ndl"
