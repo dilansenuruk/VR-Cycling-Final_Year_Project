078dad225f4cb4241cbfc77d7fd65d6cd261d05a
NaduniDamsariLiyanage <81697428+NaduniDamsariLiyanage@users.noreply.github.com>
2024-04-11 12:21:28 +0530

    test

--- new_server.js
+++ new_server.js
@@ -36,8 +36,8 @@ server.on('message', (message, senderInfo) => {
     }
     
     else if (messageString === 'create a game room') {
-        clientsOculus.length = 0;
-        clientsRasp.length = 0;
+        broadcastRpi('game created');
+        console.log(clientsRasp)
         // setTimeout(() => {
         //     clientsOculus.length = 0;
         //     clientsRasp.length = 0;
@@ -51,7 +51,7 @@ server.on('message', (message, senderInfo) => {
         
         setTimeout(() => {
             console.log("Timeout reached. Sending 'Start' message.");
-            sendPlayerNames(clientsOculus);
+            
             console.log("player names sent")
             setTimeout(() => {
                 
@@ -60,7 +60,8 @@ server.on('message', (message, senderInfo) => {
                 startMessageSent = true;
             }, 2000);
             broadcast(noOfPlayers);
-            startMessageSent = true;
+            sendPlayerNames(clientsOculus);
+            //startMessageSent = true;
 
         }, 20000);
     }
@@ -68,58 +69,96 @@ server.on('message', (message, senderInfo) => {
     else if (messageString.endsWith(':ready')) {
         // Check if the client's address is already in the list
         const playerName = messageString.split(":")[1]
+
         if (messageString.startsWith('O')){
-            const clientOculusExists = clientsOculus.some(client => 
-                client.address === senderInfo.address && client.port === senderInfo.port
-            );
-    
-            if (!clientOculusExists) {
-                // If the client is not in the list, add information about the client
+            // Check if the client's player name already exists in the list
+            const indexOculus = clientsOculus.findIndex(client => client.playerName === playerName);
+
+            if (indexOculus !== -1) {
+                // If the player name already exists, update the client's sender info
+                clientsOculus[indexOculus].address = senderInfo.address;
+                clientsOculus[indexOculus].port = senderInfo.port;
+                console.log(`Updated Oculus Client at ${senderInfo.address}:${senderInfo.port}.`);
+            } else {
+                // If the player name does not exist, add information about the client
                 clientsOculus.push({
                     playerName: playerName,
                     address: senderInfo.address,
                     port: senderInfo.port
                 });
-    
-                //console.log("Updated clients array:", clientsOculus);
-                console.log(`Oculus Client at ${senderInfo.address}:${senderInfo.port} is ready.`);
-                
-                // Send an acknowledgment back to the client
-                // server.send('ack', senderInfo.port, senderInfo.address, () => {
-                //     console.log(`Acknowledgment sent to oculus ${senderInfo.address}:${senderInfo.port}`);
-                // });
-                
-            } else {
-                console.log(`Oculus Client at ${senderInfo.address}:${senderInfo.port} is already in the list.`);
-                //server.send('ha ha', senderInfo.port, senderInfo.address)
+                console.log(`Added new Oculus Client at ${senderInfo.address}:${senderInfo.port}.`);
+                console.log("Updated clients array:", clientsOculus);
             }
+
+            // const clientOculusExists = clientsOculus.some(client => 
+            //     client.address === senderInfo.address && client.port === senderInfo.port
+            // );
+    
+            // if (!clientOculusExists) {
+            //     // If the client is not in the list, add information about the client
+            //     clientsOculus.push({
+            //         playerName: playerName,
+            //         address: senderInfo.address,
+            //         port: senderInfo.port
+            //     });
+    
+            //     //console.log("Updated clients array:", clientsOculus);
+            //     console.log(`Oculus Client at ${senderInfo.address}:${senderInfo.port} is ready.`);
+                
+            //     // Send an acknowledgment back to the client
+            //     // server.send('ack', senderInfo.port, senderInfo.address, () => {
+            //     //     console.log(`Acknowledgment sent to oculus ${senderInfo.address}:${senderInfo.port}`);
+            //     // });
+                
+            // } else {
+            //     console.log(`Oculus Client at ${senderInfo.address}:${senderInfo.port} is already in the list.`);
+            //     //server.send('ha ha', senderInfo.port, senderInfo.address)
+            // }
         }
         
         if (messageString.startsWith('R')){
-            const clientRaspExists = clientsRasp.some(client => 
-                client.address === senderInfo.address && client.port === senderInfo.port
-            );
-    
-            if (!clientRaspExists) {
-                // If the client is not in the list, add information about the client
+            const indexRpi = clientsRasp.findIndex(client => client.playerName === playerName);
+
+            if (indexRpi !== -1) {
+                // If the player name already exists, update the client's sender info
+                clientsRasp[indexRpi].address = senderInfo.address;
+                clientsRasp[indexRpi].port = senderInfo.port;
+                console.log(`Updated Rpi Client at ${senderInfo.address}:${senderInfo.port}.`);
+            } else {
+                // If the player name does not exist, add information about the client
                 clientsRasp.push({
                     playerName: playerName,
                     address: senderInfo.address,
                     port: senderInfo.port
                 });
-    
+                console.log(`Added new Rpi Client at ${senderInfo.address}:${senderInfo.port}.`);
                 console.log("Updated clients array:", clientsRasp);
-                console.log(`Rasp Client at ${senderInfo.address}:${senderInfo.port} is ready.`);
-                
-                // Send an acknowledgment back to the client
-                // server.send('ack', senderInfo.port, senderInfo.address, () => {
-                //     console.log(`Acknowledgment sent to rasp ${senderInfo.address}:${senderInfo.port}`);
-                // });
-                
-            } else {
-                console.log(`Rasp Client at ${senderInfo.address}:${senderInfo.port} is already in the list.`);
-                //server.send('ha ha', senderInfo.port, senderInfo.address)
             }
+
+            // const clientRaspExists = clientsRasp.some(client => 
+            //     client.address === senderInfo.address && client.port === senderInfo.port
+            // );
+    
+            // if (!clientRaspExists) {
+            //     // If the client is not in the list, add information about the client
+            //     clientsRasp.push({
+            //         playerName: playerName,
+            //         address: senderInfo.address,
+            //         port: senderInfo.port
+            //     });
+    
+            //     console.log("Updated clients array:", clientsRasp);
+            //     console.log(`Rasp Client at ${senderInfo.address}:${senderInfo.port} is ready.`);
+                
+            //     // Send an acknowledgment back to the client
+            //     // server.send('ack', senderInfo.port, senderInfo.address, () => {
+            //     //     console.log(`Acknowledgment sent to rasp ${senderInfo.address}:${senderInfo.port}`);
+            //     // });
+                
+            // } else {
+            //     console.log(`Rasp Client at ${senderInfo.address}:${senderInfo.port} is already in the list.`);
+            //     //server.send('ha ha', senderInfo.port, senderInfo.address)
+            // }
         }
     } 
     else if (messageString === 'disconnect') {
